660f9f9ebcbdf93371f6075fcb3f4b81844b4116
MahathirMohamed <s92066684@ousl.lk>
2023-12-05 21:01:07 +0530

    Beta v3.3

--- user/index.php
+++ user/index.php
@@ -2,32 +2,28 @@
 session_start();
 error_reporting(0);
 include("include/config.php");
+
 if (isset($_POST['submit'])) {
-	$puname = $_POST['username'];
-	$ppwd = md5($_POST['password']);
-	$ret = mysqli_query($con, "SELECT * FROM users WHERE email='$puname' and password='$ppwd'");
-	$num = mysqli_fetch_array($ret);
-	if ($num > 0) {
-		$_SESSION['login'] = $_POST['username'];
-		$_SESSION['id'] = $num['id'];
-		$pid = $num['id'];
-		$host = $_SERVER['HTTP_HOST'];
-		$uip = $_SERVER['REMOTE_ADDR'];
-		$status = 1;
-		// For stroing log if user login successfull
-		$log = mysqli_query($con, "insert into userlog(uid,username,userip,status) values('$pid','$puname','$uip','$status')");
-		header("location:home-dashboard.php");
-	} else {
-		// For stroing log if user login unsuccessfull
-		$_SESSION['login'] = $_POST['username'];
-		$uip = $_SERVER['REMOTE_ADDR'];
-		$status = 0;
-		mysqli_query($con, "insert into userlog(username,userip,status) values('$puname','$uip','$status')");
-		$_SESSION['errmsg'] = "Invalid Username or Password";
-		header("location:index.php");
-	}
+    $puname = $_POST['username'];
+    $ppwd = md5($_POST['password']);
+    $ret = mysqli_query($con, "SELECT * FROM users WHERE email='$puname' and password='$ppwd'");
+    $num = mysqli_fetch_array($ret);
+
+    if ($num > 0) {
+        $_SESSION['login'] = $_POST['username'];
+        $_SESSION['id'] = $num['id'];
+        $pid = $num['id'];
+        $uip = $_SERVER['REMOTE_ADDR'];
+        header("location:home-dashboard.php");
+    } else {
+        $_SESSION['login'] = $_POST['username'];
+        $uip = $_SERVER['REMOTE_ADDR'];
+        $_SESSION['errmsg'] = "Invalid Username or Password";
+        header("location:index.php");
+    }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
